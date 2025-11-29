adf083c04a5be1b4583b6e49af557bb1246bc53f
DaniilKlykov <x457wk1r@gmail.com>
2025-11-29 22:13:51 +0400

    first simple example

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context = {
+        'title': 'DarkMoon',
+        'content': 'Главная страница магазина DarkMoon',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'bool': True
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse('About page')
